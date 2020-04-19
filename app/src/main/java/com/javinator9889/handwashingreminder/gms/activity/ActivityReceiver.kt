3ef3d035a6d5b3f94cbebf5bdd5e4d5a8259f759
Javinator9889 <dev@javinator9889.com>
2020-04-19 11:09:51 +0200

    Updated ActivityReceiver for handling notifications in a coroutine
    ActivityReceiver now launches a coroutine (using GlobalScope) so that EmojiCompat can be initialized correctly without blocking main UI thread. When initialized, it creates the expected notification on the UI thread

--- app/src/main/java/com/javinator9889/handwashingreminder/gms/activity/ActivityReceiver.kt
+++ app/src/main/java/com/javinator9889/handwashingreminder/gms/activity/ActivityReceiver.kt
@@ -21,22 +21,24 @@ package com.javinator9889.handwashingreminder.gms.activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.annotation.StringRes
+import androidx.emoji.text.EmojiCompat
 import com.google.android.gms.location.ActivityTransition
 import com.google.android.gms.location.ActivityTransitionResult
 import com.google.android.gms.location.DetectedActivity
 import com.javinator9889.handwashingreminder.R
-import com.javinator9889.handwashingreminder.application.HandwashingApplication
+import com.javinator9889.handwashingreminder.emoji.EmojiLoader
 import com.javinator9889.handwashingreminder.notifications.NotificationsHandler
 import com.javinator9889.handwashingreminder.utils.ACTIVITY_CHANNEL_ID
+import kotlinx.coroutines.*
 
 class ActivityReceiver : BroadcastReceiver() {
-
     /**
      * {@inheritDoc}
      */
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val app = HandwashingApplication.getInstance()
+    override fun onReceive(context: Context, intent: Intent) {
         if (ActivityTransitionResult.hasResult(intent)) {
+            val emojiLoader = EmojiLoader.get(context)
             val result = ActivityTransitionResult.extractResult(intent)!!
             for (event in result.transitionEvents) {
                 if (event.transitionType !=
@@ -45,19 +47,85 @@ class ActivityReceiver : BroadcastReceiver() {
                 )
                     continue
                 val notificationHandler = NotificationsHandler(
-                    app,
+                    context,
                     ACTIVITY_CHANNEL_ID,
-                    app.getString(R.string.activity_notification_channel_name),
-                    app.getString(R.string.activity_notification_channel_desc)
+                    context.getString(
+                        R.string.activity_notification_channel_name
+                    ),
+                    context.getString(
+                        R.string.activity_notification_channel_desc
+                    )
                 )
-                notificationHandler.createNotification(
-                    R.drawable.ic_handwashing_icon,
-                    R.drawable.handwashing_app_logo,
-                    R.string.test_notification,
-                    R.string.test_content
+                putNotification(
+                    notificationHandler,
+                    emojiLoader,
+                    event.activityType,
+                    context
                 )
                 break
             }
         }
     }
+
+    private fun putNotification(
+        notificationsHandler: NotificationsHandler,
+        emojiLoader: CompletableDeferred<EmojiCompat>,
+        detectedActivity: Int,
+        context: Context,
+        coroutineScope: CoroutineScope = GlobalScope
+    ) {
+        val result = goAsync()
+        coroutineScope.launch {
+            try {
+                val notificationContent = when (detectedActivity) {
+                    DetectedActivity.WALKING ->
+                        NotificationContent(
+                            R.string.activity_notification_walk,
+                            R.string.activity_notification_walk_content
+                        )
+                    DetectedActivity.RUNNING ->
+                        NotificationContent(
+                            R.string.activity_notification_run,
+                            R.string.activity_notifications_run_content
+                        )
+                    DetectedActivity.ON_BICYCLE ->
+                        NotificationContent(
+                            R.string.activity_notification_cycling,
+                            R.string.activity_notification_cycling_content
+                        )
+                    DetectedActivity.IN_VEHICLE ->
+                        NotificationContent(
+                            R.string.activity_notification_vehicle,
+                            R.string.activity_notification_vehicle_content
+                        )
+                    else -> throw IllegalArgumentException(
+                        "Activity not recognized"
+                    )
+                }
+                val emojiCompat = emojiLoader.await()
+                val title = emojiCompat.process(
+                    context.getText(notificationContent.title)
+                )
+                val content = emojiCompat.process(
+                    context.getText(notificationContent.content)
+                )
+                withContext(Dispatchers.Main) {
+                    notificationsHandler.createNotification(
+                        R.drawable.ic_stat_handwashing,
+                        R.drawable.handwashing_app_logo,
+                        title,
+                        content,
+                        longContent = content
+                    )
+                }
+            } finally {
+                result.finish()
+            }
+        }
+    }
+
+    private data class NotificationContent(
+        @StringRes val title: Int,
+        @StringRes val content: Int
+    )
 }
